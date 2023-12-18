6512c1d6552f6e983a88901aaa18a16cca0c5340
Tomasz Leszczynski <tomi@example.com>
2023-12-18 16:03:11 +0100

    poprawki

--- resources/views/include/include.blade.php
+++ resources/views/include/include.blade.php
@@ -1,15 +1,15 @@
 
- <link href="{{asset('./public/css/app_1810.css')}}" rel="stylesheet">
- <link href="{{asset('./public/css/app_1410.css')}}" rel="stylesheet">
- <link href="{{asset('./public/css/app_1210.css')}}" rel="stylesheet">
- <link href="{{asset('./public/css/app_700.css')}}" rel="stylesheet">
- <link href="{{asset('./public/css/app_360.css')}}" rel="stylesheet">
-  <link href="{{asset('./public/css/app.css')}}" rel="stylesheet">
-     <link href="{{asset('./public/css/app_color.css')}}" rel="stylesheet">
+ <link href="{{asset('../public/css/app_1810.css')}}" rel="stylesheet">
+ <link href="{{asset('../public/css/app_1410.css')}}" rel="stylesheet">
+ <link href="{{asset('../public/css/app_1210.css')}}" rel="stylesheet">
+ <link href="{{asset('../public/css/app_700.css')}}" rel="stylesheet">
+ <link href="{{asset('../public/css/app_360.css')}}" rel="stylesheet">
+  <link href="{{asset('../public/css/app.css')}}" rel="stylesheet">
+     <link href="{{asset('../public/css/app_color.css')}}" rel="stylesheet">
  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script src="{{ asset('./public/jquery/jquery-3.6.3.js') }}"></script>  
+<script src="{{ asset('../public/jquery/jquery-3.6.3.js') }}"></script>  
  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-color/2.1.2/jquery.color.min.js"></script> 
-    <script src="{{ asset('./public/js/app.js') }}"></script>   
+    <script src="{{ asset('../public/js/app.js') }}"></script>   
